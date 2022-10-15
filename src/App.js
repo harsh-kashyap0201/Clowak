0daab564fa9c7f2f24be45e5716a7b695e9dffca
harsh <harshkashyap2012002@gmail.com>
2022-10-15 17:54:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [Users, fetchUsers] = useState([])
+
+  const userData = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => {
+            fetchUsers(json)
+      });
+  }
+
+  useEffect(() => {
+    userData()
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <div className="row">
+        {Users.map((element,i) => {
+            return <div key={i} className="col-md-3">
+                <div className="card mx-2 my-2">
+                  <img src={`https://avatars.dicebear.com/api/avataaars/${i}.svg`} className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h6 className="card-title">{element.name}</h6>
+                    <div className="list">
+                      <li className="details"><i class="fa-regular fa-envelope mx-1"></i>  {element.email}</li>
+                      <li className="details"><i class="fa-solid fa-phone-flip mx-1"></i>  {element.phone}</li>
+                      <li className="details"><i class="fa-solid fa-globe mx-1"></i>  {element.website}</li>
+                    </div>
+                  </div>
+                  <div className="card-footer">
+                    <div className="bottomButton col-md-4"> 
+                      <i className="fa-regular fa-heart heart"></i>
+                    </div>
+                    <div className="bottomButton col-md-4">
+                      <i className="fa-solid fa-pen"></i>
+                    </div>
+                    <div className="bottomButton col-md-4">
+                      <i className="fa-solid fa-trash"></i>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        })}
+      </div>
+      </div> 
     </div>
   );
 }
